28a30052278ba93fe4bdcfbfb7c6dd5e8c52ad7f
Ersin Köseoğlu <ersinkoseoglu38@gmail.com>
2023-09-29 10:57:51 +0300

    ResourcesUI

---
+++ BuilderDefender/Assets/Scripts/ResourcesUI.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourcesUI : MonoBehaviour
+{
+    [SerializeField] private Transform resourceTemplate;
+    private ResourceTypeListSO resourceTypeList;
+    private Dictionary<ResourceTypeSO, Transform> resourceTypeTransformDictionary;
+    private void Awake()
+    {
+        resourceTypeTransformDictionary = new Dictionary<ResourceTypeSO, Transform>();
+        resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
+        resourceTemplate.gameObject.SetActive(false);
+
+        int index = 0;
+        foreach (ResourceTypeSO resourceType in resourceTypeList.list)
+        {
+            Transform resourceTransform = Instantiate(resourceTemplate, transform);
+            resourceTransform.gameObject.SetActive(true);
+
+            float offsetAmount = -160f;
+            resourceTransform.GetComponent<RectTransform>().anchoredPosition = new Vector2(offsetAmount * index, 0f);
+
+
+            resourceTransform.Find("image").GetComponent<Image>().sprite = resourceType.sprite;
+
+
+            resourceTypeTransformDictionary[resourceType] = resourceTransform;
+
+            index++;
+        }
+    }
+    private void Start()
+    {
+        ResourceManager.instance.OnResourceAmountChanged += ResourceManager_OnResourceAmountChanged;
+    }
+
+    private void ResourceManager_OnResourceAmountChanged(object sender, System.EventArgs e)
+    {
+        UpdateResourceAmount();
+    }
+
+    private void UpdateResourceAmount()
+    {
+        foreach (ResourceTypeSO resourceType in resourceTypeList.list)
+        {
+            Transform resourceTransform = resourceTypeTransformDictionary[resourceType];
+
+            int resourceAmount = ResourceManager.instance.GetResourceAmount(resourceType);
+            resourceTransform.Find("text").GetComponent<TextMeshProUGUI>().SetText(resourceAmount.ToString());
+        }
+    }
+}
